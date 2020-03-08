c83312b24101bb468c4580dfcba4271a00f5fbb0
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-03-08 21:50:10 +1300

    Add project files.

---
+++ TravelRecordApp/TravelRecordApp/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    public partial class App : Application
+    {
+        public static string DatabaseLocation = string.Empty;
+
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://travelrecordapprfp.azurewebsites.net");
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string databaseLocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            DatabaseLocation = databaseLocation;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Helpers
+{
+    class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "IYVL35CWEOAXG0EGMW0US5FW1T4HGUSTZAAJUHMN4INM3QOH";
+        public const string CLIENT_SECRET = "WJ0A2C4LYITNIAGERMLGAMHKYTGWI3FDAJYGCXM4DEF1SG30";
+    }
+}
